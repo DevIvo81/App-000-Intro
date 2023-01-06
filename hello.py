5cdf06f13b759027073e87734b3d728cc553c812
DevIvo81 <izpy81@gmail.com>
2023-01-06 21:22:03 +0100

    init repo

---
+++ hello.py
@@ -0,0 +1,8 @@
+import os
+os.system("cls")
+
+os.chdir("D:/000_PyProjects/2-Udemy-OOP-Projects/")
+
+print(os.getcwd())
+
+print("Hello World")
---
+++ turtle_intro.py
@@ -0,0 +1,18 @@
+import turtle
+
+myTurtle = turtle.Turtle()
+
+# Go to certain coordinate
+myTurtle.penup()
+myTurtle.goto(50, 75)
+
+myTurtle.pendown()
+myTurtle.forward(100)
+myTurtle.left(90)
+myTurtle.forward(200)
+myTurtle.left(90)
+myTurtle.forward(100)
+myTurtle.left(90)
+myTurtle.forward(200)
+
+turtle.done()
